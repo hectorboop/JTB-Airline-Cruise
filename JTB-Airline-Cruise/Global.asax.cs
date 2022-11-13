929489770c8a53d947eab9feeb1e463759ddf0a5
Rushane WIlson <rushwilson1@gmail.com>
2022-11-13 07:18:59 -0500

    Updated Dependencies

--- JTB-Airline-Cruise/Global.asax.cs
+++ JTB-Airline-Cruise/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.Http;
+using System.Web.UI;
 
 namespace JTB_Airline_Cruise
 {
@@ -14,6 +15,16 @@ namespace JTB_Airline_Cruise
     {
         void Application_Start(object sender, EventArgs e)
         {
+            ScriptManager.ScriptResourceMapping.AddDefinition("bootstrap",
+            new ScriptResourceDefinition
+            {
+                Path = "~/Scripts/bootstrap.min.js",
+                DebugPath = "~/Scripts/bootstrap.js",
+                CdnPath = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js",
+                CdnSupportsSecureConnection = true,
+                LoadSuccessExpression = "window.jQuery.fn.carousel"
+            });
+
             // Code that runs on application startup
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
