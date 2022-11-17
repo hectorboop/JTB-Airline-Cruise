106cc52f61401edd2a3eec3ac0b7ecf227482ad5
Rushane WIlson <rushwilson1@gmail.com>
2022-11-17 07:53:59 -0500

    Final Default UI

---
+++ JTB-Airline-Cruise/App/Airline.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JTB_Airline_Cruise.App
+{
+    public class Airline
+    {
+        public int AirlineID { get; set; }
+        public string AirlineName { get; set; }
+        public virtual List<Flight> Flights { get; set; }
+    }
+}
