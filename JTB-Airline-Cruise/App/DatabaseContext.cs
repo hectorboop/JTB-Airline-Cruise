7a228edf447989f54f5b4773f1ea23b88fb6065a
Rushane WIlson <rushwilson1@gmail.com>
2022-11-17 07:53:59 -0500

    Final Default UI

---
+++ JTB-Airline-Cruise/App/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace JTB_Airline_Cruise.App
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("JTB_DB")
+        {
+            //Database.SetInitializer<DatabaseContext>(new AppInitializer())
+        }
+    }
+}
