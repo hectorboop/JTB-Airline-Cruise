49eed423f22ca83df4180fcf526fd8ea73ce5880
Rushane WIlson <rushwilson1@gmail.com>
2022-11-25 22:04:10 -0500

    Fixing Merge Conflict

---
+++ JTB-Airline-Cruise/App/Flight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JTB_Airline_Cruise.App
+{
+    public class Flight
+    {
+        public int FlightID { get; set; }
+        public virtual Airline Airline { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public int Travelers { get; set; }
+        public string FlightType { get; set; }
+        public string FlightClass { get; set; }
+        public float FlightPrice { get; set; }
+
+    }
+}
